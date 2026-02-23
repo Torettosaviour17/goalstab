d8fae46f3ba6ea52600d7a3785253087c1e1bcb5
Torettosaviour17 <saviourchidubem17@gmail.com>
2026-02-23 16:56:16 +0100

    added auto sav

---
+++ src/components/goals/AutoSaveIndicator.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="flex items-center gap-2 text-sm">
+    <span class="text-gray-400">⚡</span>
+    <span class="text-gray-300">
+      Auto-save:
+      <span class="font-medium text-white">
+        {{
+          goal.type === "percentage"
+            ? `${goal.autoSave}%`
+            : `₦${formatNumber(goal.autoSave)}`
+        }}
+      </span>
+      /{{ goal.frequency }}
+    </span>
+  </div>
+</template>
+
+<script setup lang="ts">
+import type { Goal } from "@/types/goal";
+
+defineProps<{
+  goal: Goal;
+}>();
+
+const formatNumber = (num: number) => new Intl.NumberFormat().format(num);
+</script>
