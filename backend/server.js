2e34e42e31db69896c2e8e3bab832bf904896a4f
Torettosaviour17 <saviourchidubem17@gmail.com>
2026-03-02 11:59:00 +0100

    sti;; working on backend deployment 2

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,9 @@
-require('dotenv').config();
-const app = require('./src/app');
+javascript;
+require("dotenv").config();
+const app = require("./src/app");
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running on port ${PORT}`);
 });
